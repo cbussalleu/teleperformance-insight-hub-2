13704da5fc072467addb933d9223cd7e5e074f4e
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-05-31 02:17:49 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -18,24 +15,20 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/historia" element={<Historia />} />
-          <Route path="/expectativas" element={<Expectativas />} />
-          <Route path="/percepcion" element={<Percepcion />} />
-          <Route path="/ciclo-vida" element={<CicloVida />} />
-          <Route path="/inclusion" element={<Inclusion />} />
-          <Route path="/competencia" element={<Competencia />} />
-          <Route path="/reposicionamiento" element={<Reposicionamiento />} />
-          <Route path="/conclusiones" element={<Conclusiones />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/historia" element={<Historia />} />
+        <Route path="/expectativas" element={<Expectativas />} />
+        <Route path="/percepcion" element={<Percepcion />} />
+        <Route path="/ciclo-vida" element={<CicloVida />} />
+        <Route path="/inclusion" element={<Inclusion />} />
+        <Route path="/competencia" element={<Competencia />} />
+        <Route path="/reposicionamiento" element={<Reposicionamiento />} />
+        <Route path="/conclusiones" element={<Conclusiones />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
