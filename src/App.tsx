e2728ea6fdfa76d9bf64f2500193b9f71b25656d
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-05-31 02:43:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Metodologia from "./pages/Metodologia";
 import Historia from "./pages/Historia";
 import Expectativas from "./pages/Expectativas";
 import Percepcion from "./pages/Percepcion";
 import CicloVida from "./pages/CicloVida";
-import Factores from "./pages/Factores";
-import RSE from "./pages/RSE";
+import Inclusion from "./pages/Inclusion";
 import Competencia from "./pages/Competencia";
 import Reposicionamiento from "./pages/Reposicionamiento";
-import Industria from "./pages/Industria";
 import Conclusiones from "./pages/Conclusiones";
 import NotFound from "./pages/NotFound";
 
@@ -21,27 +15,20 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/metodologia" element={<Metodologia />} />
-          <Route path="/historia" element={<Historia />} />
-          <Route path="/expectativas" element={<Expectativas />} />
-          <Route path="/percepcion" element={<Percepcion />} />
-          <Route path="/ciclo-vida" element={<CicloVida />} />
-          <Route path="/factores" element={<Factores />} />
-          <Route path="/rse" element={<RSE />} />
-          <Route path="/competencia" element={<Competencia />} />
-          <Route path="/reposicionamiento" element={<Reposicionamiento />} />
-          <Route path="/industria" element={<Industria />} />
-          <Route path="/conclusiones" element={<Conclusiones />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/historia" element={<Historia />} />
+        <Route path="/expectativas" element={<Expectativas />} />
+        <Route path="/percepcion" element={<Percepcion />} />
+        <Route path="/ciclo-vida" element={<CicloVida />} />
+        <Route path="/inclusion" element={<Inclusion />} />
+        <Route path="/competencia" element={<Competencia />} />
+        <Route path="/reposicionamiento" element={<Reposicionamiento />} />
+        <Route path="/conclusiones" element={<Conclusiones />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
